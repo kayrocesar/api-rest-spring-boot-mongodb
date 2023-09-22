9bed2087ef6f6d0f5c8dddf2e04fb9253d14ce3d
kayrocesar <kayro.cesar.kc@gmail.com>
2023-09-22 18:36:26 -0300

    Obtendo um post por id

---
+++ src/main/java/com/kayrocesar/workshopmongo/services/PostService.java
@@ -0,0 +1,29 @@
+package com.kayrocesar.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kayrocesar.workshopmongo.domain.Post;
+import com.kayrocesar.workshopmongo.repository.PostRepository;
+import com.kayrocesar.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public List<Post> findAll() {
+		return repo.findAll();
+	}
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+
+}
