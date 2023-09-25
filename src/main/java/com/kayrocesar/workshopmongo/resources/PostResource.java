d56caa9961c0d2495f561cb86440bbca1bfa9adb
kayrocesar <kayro.cesar.kc@gmail.com>
2023-09-25 14:25:54 -0300

    Consulta simples com query methods

--- src/main/java/com/kayrocesar/workshopmongo/resources/PostResource.java
+++ src/main/java/com/kayrocesar/workshopmongo/resources/PostResource.java
@@ -3,17 +3,22 @@ package com.kayrocesar.workshopmongo.resources;
 
 
 
+import java.net.URI;
+import java.net.URLDecoder;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
 import com.kayrocesar.workshopmongo.domain.Post;
-
+import com.kayrocesar.workshopmongo.resources.util.URL;
 import com.kayrocesar.workshopmongo.services.PostService;
 
 
@@ -29,7 +34,12 @@ public class PostResource {
 		Post post = service.findById(id);
 		return ResponseEntity.ok().body(post);
 	}
-	
+	@GetMapping(value="/titlesearch")
+	public ResponseEntity <List<Post>> findByTitle(@RequestParam(value="text",defaultValue = "") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
 	
 	
 	
--- src/main/java/com/kayrocesar/workshopmongo/services/PostService.java
+++ src/main/java/com/kayrocesar/workshopmongo/services/PostService.java
@@ -26,4 +26,7 @@ public class PostService {
 	}
 
 
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
 }
